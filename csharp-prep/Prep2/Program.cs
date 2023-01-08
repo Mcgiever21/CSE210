e561a277325a7f8f94640b880e6d4af3384b7466
Mcgiever21 <mkwahl25@byui.edu>
2023-01-07 22:28:26 -0700

    changed to grade reader with passing values

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("input score: ");
+        string value = Console.ReadLine();
+        int score = int.Parse(value);
+        Program.grade(score);
+    }
+ 
+    static void grade(int score)
+    {
+        int a = score;
+        string letter;
+        if (a>95)
+        {
+            letter = "A+";
+        }
+        else if (a==95){letter = "A";}
+        else if (a>=90){letter = "A-";}
+        else if (a>85){letter="B+";}
+        else if (a==85){letter="B";}
+        else if (a>=80){letter="B-";}
+        else if (a>75){letter="C+";}
+        else if (a==75){letter="C";}
+        else if (a>=70){letter="C-";}
+        else if (a>=60){letter="D";}
+        else
+        {letter = "F";}
+        Console.WriteLine(letter);
+        return;
     }
 }
