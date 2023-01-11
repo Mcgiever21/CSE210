c5b26ead51227ac1ccc298cdb6905d53793a6bba
Mcgiever21 <mkwahl25@byui.edu>
2023-01-11 10:50:03 -0700

    number list and sorted fixed for readablility

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,73 +6,89 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
-        int a=1;
-        while (a>0)
+
+        int looperVariable = 1;
+
+        while (looperVariable>0)
         {
-            List<int> ListA = Program.numberEnter();
+            List<int> baseNumberList = Program.numberEnter();
             Console.WriteLine("Do you want the list sorted?");
             string answer = Console.ReadLine();
+
             if(answer == "yes"){
-                List<int> listSorted = new List<int>(Program.sorter(listA));
+                List<int> numberlistSorted = new List<int>(Program.sorter(baseNumberList));
+                Console.WriteLine(numberlistSorted);
             }
-            Program.math(listA);
+            Program.math(baseNumberList);
             Console.WriteLine("Would you like to run another list?");
-            string B = Console.ReadLine();
-            if (B=="no"){a=0;}
+
+            answer = Console.ReadLine();
+
+            if (answer == "no" ) { looperVariable = 0 ; }
         }
     }
 
     static List<int> numberEnter()
     {
-        List<int> listA = new List<int>();
+        List<int> baseNumberList = new List<int>();
         Console.WriteLine("Enter numbers one at a time at promt, enter 0 when finished.");
+
         int A;
         do
         {
             Console.WriteLine("Enter a number: ");
             A = int.Parse(Console.ReadLine());
-            listA.Add(A);
+            baseNumberList.Add(A);
 
-        } while (A!=0);
-        Console.WriteLine($"total number of items in list: {listA.Count}");
-        return listA;
+        } while (A != 0);
+
+        Console.WriteLine($"total number of items in list: {baseNumberList.Count}");
+
+        return baseNumberList;
     }
     
-    static List<int> sorter(List<int> listA)
+    static List<int> sorter(List<int> baseNumberList)
     {
-        List<int> listC = new List<int>();
-        int len = listA.Count();
+        List<int> listsorted = new List<int>();
+        int len = baseNumberList.Count();
+        int i = 1;
         do
         {
-            for (int i = 0; i < listA.Count; i++){
-                listC.Add(listA(i));
-            }
+            listsorted.Add(baseNumberList[i]);
+            i += 1;
+            len -= 1;
+            
         }while(len>0);
-        listC.Sort();
-        return listC;
+        listsorted.Sort();
+        return listsorted;
     }
-    static void math(List<int> listA)
+    static void math(List<int> baseNumberList)
     {
-        int sum=0;
-        int n=0;
-        int largest=-10000;
-        int smallest=10000;
-        for (int i=0;i<listA.Count;i++){
-            sum =+listA(i);
-            n=+1;
-            if (listA(i)>largest){
-                largest=listA(i);
+        int sum = 0;
+        int n = 0;
+        int largest = -10000;
+        int smallest = 10000;
+
+        for (int i = 0 ; i<baseNumberList.Count ; i++){
+            sum += baseNumberList[i];
+            n += 1;
+
+            if (baseNumberList[i]>largest){
+                largest = baseNumberList[i];
             }
-            if (listA(i)<smallest){
-                smallest=listA(i);
+
+            if (baseNumberList[i]<smallest){
+                smallest = baseNumberList[i];
             }
         }
+
         Console.WriteLine($"sum of list = {sum}");
+
         int avg = sum/n;
+
         Console.WriteLine($"List Average = {avg}");
         Console.WriteLine($"the largest number is {largest}");
         Console.WriteLine($"the smallest number is {smallest}");
-        return;
     }
     
 }
